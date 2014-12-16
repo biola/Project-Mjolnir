35ef71be183cd1ee7bef663a9c49e6e278d81123
Michael Stephens <michael.a.stephens@biola.edu>
2014-12-16 13:04:03 -0800

    Added features to the carousel

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,8 @@ class EventsController < ApplicationController
     @events = policy_scope(Event).published
     @events = @events.custom_search(params[:q]) if params[:q]
     # @events = @events.order('start_date DESC').page(params[:page]).per(25)
+
+    @features = Feature.any_of(site_ids: "54907af24d69631517000000")
   end
 
   private
