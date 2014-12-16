9ffac362894753e7930b53529f1499847d5e6840
Michael Stephens <michael.a.stephens@biola.edu>
2014-12-16 14:00:21 -0800

    Added the news side of the features. whoops

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,8 @@ class ArticlesController < ApplicationController
     @articles = policy_scope(Article).published
     @articles = @articles.custom_search(params[:q]) if params[:q]
     # @articles = @articles.order('start_date DESC').page(params[:page]).per(25)
+
+    @features = Feature.any_of(site_ids: "54907af24d69631517000000")
   end
 
   private
